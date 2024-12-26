45ef9148c52c227cb6eaa76777a8c54b494eb581
rain44556 <bristy44556@gmail.com>
2024-12-26 16:20:40 +0600

    added learnwithexperts section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import PopularServices from '../components/popularServices';
+import LearnWithExperts from '../components/LearnWithExperts';
+import SuccessStories from '../components/SuccessStories';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <PopularServices></PopularServices>
+           <LearnWithExperts></LearnWithExperts>
+           <SuccessStories></SuccessStories>
         </div>
     );
 };
